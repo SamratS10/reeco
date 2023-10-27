b9a191d91f5355a0fc1cbf47bcf9fed692eb5430
SamratS10 <130583059+SamratS10@users.noreply.github.com>
2023-10-27 09:02:13 +0530

    Update aboutDetails.js

--- src/components/aboutDetails.js
+++ src/components/aboutDetails.js
@@ -1,4 +1,22 @@
-import { Block,BlockPara,ContainerHeading } from "./style"
+import styled from "styled-components";
+
+const Block=styled.div`
+display:flex;
+flex-direction:column;
+padding:15px;
+overflow:anywhere;
+border-right:${(props)=>props.type};
+padding-right:20px
+`;
+const BlockPara=styled.p`
+color:grey;
+font-size:15px;
+font-family:"Roboto;
+`;
+const ContainerHeading = styled.h2`
+font-size:2px; 
+font-family:"Roboto";
+`;
 
 const Details = (props)=>{
     const {para,heading,type} = props
